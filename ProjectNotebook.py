5e042304c939d963dd6f07dcbe14e65aacd4ecf9
audreyrav <101620985+audreyrav@users.noreply.github.com>
2022-03-14 23:27:06 -0700

    Add files via upload

---
+++ ProjectNotebook.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Project Description
+
+# Write a brief description of your project here. 
+# 
+# Hello and welcome to my project! For this assignment, I decided to make a brief screening tool in the form of a chatbot for parents who suspect that their child might have conduct disorder (CD). At the beginning, the chatbot will ask the user to type anything in order to proceed with the test. The user will then be asked six questions about behaviors or symptoms related to conduct disorder. After that, the scores from the questions will be totaled and the user will receive a likelihood of conduct disorder. The user then will be asked whether they want to receive more information about the disorder.
+
+# ## Project Code
+# 
+# If it makes sense for your project, you can have code and outputs here in the notebook as well.
+
+# In[1]:
+
+
+from my_module.functions import cd_screener, get_help
+
+
+# In[ ]:
+
+
+cd_screener()
+get_help()
+
+
+# #### Extra Credit (*optional*)
+# 
+# 1. Before this class I had no python or coding experience at all.
+# 2. My project went above and beyond as I included 4 unique functions and 9 functions in total over the 3 required functions. I also included a youtube video, text colors, and column alignment, which was not mentioned in class. Thank you!
